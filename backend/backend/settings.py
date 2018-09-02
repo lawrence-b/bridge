0ef983302e7fd9642528b3f321cd2eb0b96c52fb
Lawrence Berry <lawrenceberry@outlook.com>
2018-09-02 13:08:38 +0100

    Updated requirements.txt and added django-storages

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'djoser',
     'corsheaders',
     'social_django',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -150,11 +151,27 @@ USE_L10N = True
 
 USE_TZ = False
 
+# Define storage options
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID'],
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY'],
+AWS_STORAGE_BUCKET_NAME = 'bridge-backend-storage'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'backend/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static')
-STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+#STATIC_URL = '/static/'
 
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
